9303146fe10ea2337ef74654614d8174d528aaea
Alexey Ershov <alex4rom@gmail.com>
2016-02-03 18:27:01 +0200

    Add gui

--- guess_num/guess.rkt
+++ guess_num/guess.rkt
@@ -1,20 +1,66 @@
 #lang racket
+(require 2htdp/universe 2htdp/image)
 
-(define lower 1)
-(define upper 100)
+(define TEXT-SIZE 10)
+(define TEXT-X 15)
+(define TEXT-UPPER-Y 20)
+(define TEXT-LOWER-Y 20)
+(define WIDTH 400)
+(define HEIGHT 400)
+(define SIZE 50)
 
-(define (guess)
-  (quotient (+ lower upper) 2))
+(struct interval (small big))
 
-(define (smaller)
-  (set! upper (max lower (sub1 (guess))))
-  (guess))
+(define HELP-TEXT
+  (text "UP for larger numbers, DOWN for smaller ones"
+        TEXT-SIZE
+        "blue"))
 
-(define (bigger)
-  (set! lower (min upper (add1 (guess))))
-  (guess))
+(define HELP-TEXT2
+  (text "Press = when your number is guessed; q to quit."
+        TEXT-SIZE
+        "blue"))
 
-(define (start n m)
-  (set! lower (min n m))
-  (set! upper (max n m))
-  (guess))
+(define COLOR "red")
+
+(define MT-SC
+  (place-image/align
+   HELP-TEXT TEXT-X TEXT-UPPER-Y "left" "top"
+   (place-image/align
+    HELP-TEXT2 TEXT-X TEXT-LOWER-Y "left" "bottom"
+    (empty-scene WIDTH HEIGHT))))
+
+(define (start lower upper)
+  (big-bang (interval lower upper)
+            (on-key deal-with-guess)
+            (to-draw render)
+            (stop-when single? render-last-scene)))
+
+(define (deal-with-guess w key)
+  (cond [(key=? key "up") (bigger w)]
+        [(key=? key "down") (smaller w)]
+        [(key=? key "q") (stop-with w)]
+        [(key=? key "=") (stop-with w)]
+        [else w]))
+
+(define (smaller w)
+  (interval (interval-small w)
+            (max (interval-small w) (sub1 (guess w)))))
+
+(define (bigger w)
+  (interval (min (interval-big w) (add1 (guess w)))
+            (interval-big w)))
+
+(define (guess w)
+  (quotient (+ (interval-small w) (interval-big w)) 2))
+
+(define (render w)
+  (overlay (text (number->string (guess w)) SIZE COLOR) MT-SC))
+
+(define (render-last-scene w)
+  (overlay (text "End" SIZE COLOR) MT-SC))
+
+(define (single? w)
+  (= (interval-small w) (interval-big w)))
+
+(start 1 100)
