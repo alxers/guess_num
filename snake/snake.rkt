4bebbe9175fbcea11f8214da0d3767900b99cf94
Alexey Ershov <alex4rom@gmail.com>
2016-02-05 17:29:53 +0200

    Add big bang module

--- snake/snake.rkt
+++ snake/snake.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require 2htdp/universe)
 
 ; structs
 ; world representation
@@ -6,3 +7,17 @@
 (struct snake (dir segs))
 (struct posn (x y))
 (struct goo (loc expire))
+
+; Launch and run game
+(define (start-snake)
+  (big-bang (pit (snake "right" (list (posn 1 1)))
+                 (list (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)))
+            (on-tick next-pit TICK-RATE)
+            (on-key direct-snake)
+            (to-draw render-pit)
+            (stop-when dead? render-end)))
